744a72c13d76a33e986e83c48e4eac82afb76adf
tensorbytes0202 <aditya11501246@gmail.com>
2026-05-11 19:36:32 +0530

    Added sorting code

---
+++ Hashing/hashmap.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<map>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    // PRECOMPUTE
+    map<int,int> mpp;
+
+    for(int i = 0; i < n; i++){
+        mpp[arr[i]]++;
+    }
+
+    int q;
+    cin >> q;
+
+    while(q--){
+
+        int number;
+        cin >> number;
+
+        // FETCH
+        cout << mpp[number] << endl;
+    }
+
+    return 0;
+}
