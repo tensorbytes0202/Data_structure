e243ebc31098fab8f9834d200780d56c6ef16d0d
Aditya sikarwar <aditya11501246@gmail.com>
2026-08-01 13:42:59 +0530

    Time: 11 ms (8.12%), Space: 11.8 MB (49.67%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        sort(ransomNote.begin(),ransomNote.end());
+        sort(magazine.begin(),magazine.end());
+        int i =0;
+        int j =0;
+        while(i< ransomNote.size() && j<magazine.size()){
+
+        if(ransomNote[i] == magazine[j]){
+            i++;
+            j++;
+        }
+        else if(ransomNote[i]>magazine[j]){
+            j++;
+        }
+        else{
+             return false;
+        }
+    }
+    return i == ransomNote.size();
+    }
+};
