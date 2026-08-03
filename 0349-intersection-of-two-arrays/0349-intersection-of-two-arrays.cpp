1f55fbd621704e8fa19ac8e4583739ac358a607c
Aditya sikarwar <aditya11501246@gmail.com>
2026-08-03 20:09:08 +0530

    Time: 0 ms (100%), Space: 14.3 MB (59.28%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -14,3 +14,8 @@ public:
 
     }
 };
+
+// //Pehle nums1 ke saare unique elements ko unordered_set me store karte hain taaki fast search kar sakein.
+// Phir nums2 ke har element ko set me check karte hain ki wo present hai ya nahi.
+// Agar element milta hai to usko answer me add kar dete hain.
+// Element ko set se remove kar dete hain taaki same element dobara answer me na aaye.
