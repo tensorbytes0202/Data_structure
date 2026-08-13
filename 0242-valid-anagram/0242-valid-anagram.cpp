65a51d63b070c4b510eb27b475322a25149da9ac
Aditya sikarwar <aditya11501246@gmail.com>
2026-08-13 23:12:19 +0530

    Time: 0 ms (100%), Space: 9.9 MB (26.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t){
-            return true;
+        unordered_map<char,int> mp;
+        if(s.length()!=t.length()){
+            return false;
         }
-        return false;
+        else{
+
+        for(char c:s){
+            mp[c]++;
+        }
+        for(char c:t){
+            if(mp[c]==0){  // maan lo s me 3 a and and t me 2 and s
+            // ko map karwana h 3rd wale a pe s ke t khatam yani 0 ho jayega to false;
+                return false; 
+            }
+            mp[c]--;
+        }
+        }
+        return true;
         
     }
 };
