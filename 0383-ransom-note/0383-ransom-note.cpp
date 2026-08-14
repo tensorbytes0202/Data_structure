722bd1030c02f7d9e495a65563f945a3f506be6a
Aditya sikarwar <aditya11501246@gmail.com>
2026-08-14 12:45:41 +0530

    Time: 3 ms (52.48%), Space: 12 MB (30.1%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-       int freq[26] = {0};
-       for(char ch : magazine){
-        freq[ch-'a']++;
-       }
-       for( char ch : ransomNote){
-        freq[ch - 'a']--;
-        if(freq[ch - 'a']<0)
-        return false;
-       }
-       return true;
+        unordered_map<char, int> mp;
+
+        for(char c : magazine) {
+            mp[c]++;
+        }
+
+        for(char c : ransomNote) {
+            if(mp[c] == 0) {
+                return false;
+            }
+
+            mp[c]--;
+        }
+
+        return true;
     }
 };
-// Pehle magazine ki frequency count kar rahe hain, fir ransom ki..."
-
-// ✅ Sahi.
-
-// Lekin ransom ki frequency alag se count nahi kar rahe.
-
-// Hum ransom ke characters ko magazine ki frequency se consume (use) kar rahe hain.
