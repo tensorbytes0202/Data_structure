905549c63a08ba68022d6d3e844cc7ee475bc797
Aditya sikarwar <aditya11501246@gmail.com>
2026-08-15 16:58:50 +0530

    Time: 1 ms (48.65%), Space: 14.5 MB (34.42%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st(nums1.begin(),nums1.end());
-        vector<int> ans;
-        for(int num : nums2){
-            if(st.count(num)){
-                ans.push_back(num);
-                st.erase(num);
-            }
+        unordered_map<int,int> mp;
+        vector<int> arr;
+        for(int i : nums1){
+            mp[i]++;
         }
-        return ans;
 
-
+        for(int i :nums2){
+            if(mp[i]>0){
+        
+        
+        arr.push_back(i);
+            mp[i]=0;
+            }
+            }
+    return arr;
     }
 };
-
-// //Pehle nums1 ke saare unique elements ko unordered_set me store karte hain taaki fast search kar sakein.
-// Phir nums2 ke har element ko set me check karte hain ki wo present hai ya nahi.
-// Agar element milta hai to usko answer me add kar dete hain.
-// Element ko set se remove kar dete hain taaki same element dobara answer me na aaye.
