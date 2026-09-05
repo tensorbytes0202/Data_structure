ac0a992aa13061bdd00e4650fc4b075eba969f9c
Aditya sikarwar <aditya11501246@gmail.com>
2026-09-05 14:59:42 +0530

    Time: 0 ms (100%), Space: 8.8 MB (88.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
+        int n = s.size();
         stack<char> st;
-        for(char c:s){
-        if(c =='('||c=='['||c=='{'){
-            st.push(c);
-        }
-        else {
-            if(st.empty()){
+        for(int i =0;i<n;i++){
+         char ch = s[i];
+         if(ch=='('||ch =='{'||ch=='['){
+            st.push(ch);
+         }else{
+            if(st.size()==0){
                 return false;
             }
-            char top = st.top();
-        
-        if((c==')'&&top!='(')||(c==']' && top!='[') ||c=='}' && top!= '{' ){
-        
-            return false;                
+            if((st.top()=='('&&ch ==')')||(st.top()=='{'&&ch=='}')||(st.top()=='['&&ch==']')){
+                st.pop();
             }
-            st.pop();
-        }
-        }
-        return st.empty();
-    
+            else{
+                return false;
+            }
+         }   
+            }
+            if(st.size()!=0){
+                return false;
+            }
+            return true;
+
     }
 };
